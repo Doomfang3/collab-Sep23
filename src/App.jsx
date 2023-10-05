5738523975d7fec588a45a60e59ea7cd0c8f8e10
Joshua <horheyinc8@gmail.com>
2023-10-05 16:17:27 +0200

    all dogs route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <>
       <h1>THe base of my project</h1>
       <Routes>
-        <Route path='/' element={<h1>HomePage</h1>} />
-
-        <Route path='*' element={<h1>404 Page</h1>} />
+        <Route path="/" element={<h1>HomePage</h1>} />
+        <Route path="/dogs" element={<h2>All dog page!</h2>} />
+        <Route path="*" element={<h1>404 Page</h1>} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
