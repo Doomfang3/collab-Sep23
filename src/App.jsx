746f9de6a79a88e9c6ca247eec3033ffb929f257
Joshua <horheyinc8@gmail.com>
2023-10-05 16:34:38 +0200

    ragnars branch added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <>
       <h1>THe base of my project</h1>
       <Routes>
-        <Route path='/' element={<h1>HomePage</h1>} />
-        <Route path='/cats' element={<h1>All the cats</h1>} />
-        <Route path='*' element={<h1>404 Page</h1>} />
+        <Route path="/" element={<h1>HomePage</h1>} />
+        <Route path="/cats" element={<h1>All the cats</h1>} />
+        <Route
+          path="/ragnar-rules"
+          element={<h1>Dogs are better than cats!</h1>}
+        />
+        <Route path="*" element={<h1>404 Page</h1>} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
