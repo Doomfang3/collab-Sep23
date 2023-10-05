af869ecd0386241260b128a163a860aab1f08026
Doomfang3 <saubinmathieu@gmail.com>
2023-10-05 16:10:27 +0200

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <>
       <h1>THe base of my project</h1>
+      <Routes>
+        <Route path='/' element={<h1>HomePage</h1>} />
+
+        <Route path='*' element={<h1>404 Page</h1>} />
+      </Routes>
     </>
   )
 }
